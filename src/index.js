191e4868ea944d9485e2fd0b74b246e807377437
gideonlangat98 <gideonlangat98@gmail.com>
2022-06-18 14:39:11 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("create-task-form")
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    addToDo(e.target.add.value)
+    form.reset()
+  })
 });
+
+function addToDo(theList) {
+  let newList = document.createElement('list')
+  let butn = document.createElement('button')
+  let task = document.createElement('tasks')
+
+  newList.innerHTML = `${list}`
+  butn.innerHTML = 'deleteToDo'
+  butn.addEventListener("click", deleteToDo)
+  newList.appendChild(butn)
+  task.appendChild(newList)
+}
+addToDo(theList);
+
+function deleteToDo(e) {
+  e.target.parentNode.remove();
+}
+
+
